d25f2fd964bd2ee3e169cd6f54b2f1d2d28d713b
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-08-30 19:41:50 +0530

    creare font and first things

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1
+      }}
+    >
+      <Text style={{
+        fontSize:30,
+        fontFamily:'outfit'
+      }}>Home Page</Text>
+    </View>
+  );
+}
